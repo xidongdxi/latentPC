9e0a92ab4f8a23769972a985d89635053278a3cd
Dong Xi <38050865+xidongdxi@users.noreply.github.com>
2022-04-04 15:16:08 -0400

    Update and rename simulation_shd.R to simulation_M3.R

--- simulation_M3.R
+++ simulation_M3.R
@@ -8,7 +8,7 @@ sim <- function(x, scen) {
   n <- scen$n[x]
   d <- scen$d[x]
   method <- scen$method[x]
-  la <- c(rep("continuous", d / 3), rep("binary", d / 3), rep("ordinal", d / 3))
+  la <- c(rep("continuous", floor(d / 3)), rep("binary", floor(d / 3)), rep("ordinal", d - 2 * floor(d / 3)))
   data <- generate_DAG(n, d, deg, label = la, lB = -0.5, uB = 0.5)
   X <- data$X
   A <- as(dag2cpdag(data$A), "matrix")
